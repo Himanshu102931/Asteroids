10e6b86aead34b396416d39501653a370723630f
Himanshu102931 <mehimanshu31@gmail.com>
2026-08-01 10:52:07 +0530

    Modified the main.py so that when asteroid and the shot collides, they both get destroyed through .kill command

--- main.py
+++ main.py
@@ -43,6 +43,13 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+
+
         screen.fill(color="Black")
         for sprite in drawable:
             sprite.draw(screen)
