566c652138a49e259a9a48b95a6c97dd5944cfc5
Himanshu102931 <mehimanshu31@gmail.com>
2026-08-01 11:12:40 +0530

    Modified the asteroid.py so that when the player shot colldes with the astroids it get split according to the size

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,8 @@
-from constants import LINE_WIDTH
+from types import new_class
+
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
+import random
 
 import pygame
 from circleshape import CircleShape
@@ -13,3 +17,19 @@ class Asteroid(CircleShape):
 
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
+
+    def split(self) -> None:
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        random.uniform(20, 50)
+        rotated_vector_1 = self.velocity.rotate(random.uniform(0, 360))
+        rotated_vector_2 = self.velocity.rotate(random.uniform(0, -360))
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = rotated_vector_1 * 1.2
+        asteroid2.velocity = rotated_vector_2 * 1.2
